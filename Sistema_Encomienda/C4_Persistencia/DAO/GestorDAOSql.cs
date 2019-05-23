77eccc211fed4eaa09a2791abfb2e8eeaa91227e
jjeyson <george3000.jj@gmail.com>
2019-05-23 12:59:06 -0500

    Modificacion de la cadena porque faltó el Password

--- Sistema_Encomienda/C4_Persistencia/DAO/GestorDAOSql.cs
+++ Sistema_Encomienda/C4_Persistencia/DAO/GestorDAOSql.cs
@@ -26,7 +26,7 @@ namespace C4_Persistencia.DAO
             try
             {
                 conexion = new SqlConnection();
-                conexion.ConnectionString = "Data Source=sql7002.site4now.net;Initial Catalog=DB_A4909B_SistemaEncomiendas;Persist Security Info=True;User ID=DB_A4909B_SistemaEncomiendas_admin";
+                conexion.ConnectionString = "Data Source=sql7002.site4now.net;Initial Catalog=DB_A4909B_SistemaEncomiendas;Persist Security Info=True;User ID=DB_A4909B_SistemaEncomiendas_admin;Password=***********";
                 conexion.Open();
             }
             catch (Exception e)
