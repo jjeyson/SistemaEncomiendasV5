b3c8b34e9216330fc2021a57c9eb7e415c63b6aa
jjeyson <george3000.jj@gmail.com>
2019-05-23 19:03:10 -0500

    Commit Creando el listar Usuario sin conexion a Base de Datos

---
+++ Sistema_Encomienda/C1_Presentacion/Controllers/UsuarioController.cs
@@ -0,0 +1,32 @@
+﻿using C3_Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace C1_Presentacion.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        //
+        // GET: /Usuario/
+        public ActionResult ListaUsuario()
+        {
+            return View();
+        }
+        
+        public ViewResult NuevoUsuario()
+        {
+            return View();
+        }
+        public ActionResult modificarCliente()
+        {
+            return View();
+        }
+        public ActionResult eliminarCliente()
+        {
+            return View();
+        }
+	}
+}
